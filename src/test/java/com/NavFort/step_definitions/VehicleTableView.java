6c71d57137ccaec457dc3d109a62160cd5eed890
Esra Satiroglu <israsatiroglu@gmail.com>
2023-04-08 12:14:00 +0100

    vehicle table view file updated

--- src/test/java/com/NavFort/step_definitions/VehicleTableView.java
+++ src/test/java/com/NavFort/step_definitions/VehicleTableView.java
@@ -1,11 +1,16 @@
 package com.NavFort.step_definitions;
 
+import com.NavFort.pages.BasePage;
+import com.NavFort.pages.LoginPage;
 import com.NavFort.pages.VehicleTableViewPage;
 import com.NavFort.utilities.BrowserUtils;
+import com.NavFort.utilities.ConfigurationReader;
 import com.NavFort.utilities.Driver;
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
@@ -15,6 +20,26 @@ import static org.junit.Assert.assertTrue;
 public class VehicleTableView  {
 
     VehicleTableViewPage vehicleTableView= new VehicleTableViewPage();
+    LoginPage loginPage=new LoginPage();
+
+    @Given("User is on the login page")
+    public void user_is_on_the_login_page() {
+
+        loginPage.goLoginPage();
+    }
+
+    @When("User logs in as a {string}")
+    public void loginAsDriver(String driver) {
+        loginPage.loginAsUserType(driver);
+    }
+
+    @Then("User is on the {string} page")
+    public void verifyPageDisplayed(String pageName) {
+        String expectedPage = "Quick Launchpad";
+        WebElement dashboardPage = BrowserUtils.waitForVisibility(By.xpath("//h1[@class='oro-subtitle']"),10);
+        String actualPage = dashboardPage.getText();
+        assertEquals(expectedPage, actualPage);
+    }
 
     @When("user hover over {string} button")
     public void user_hover_over_button(String fleetBtn) {
@@ -45,18 +70,18 @@ public class VehicleTableView  {
 
     @When("user clicks the \">\" button")
     public void clickNextPageButton() {
+        BrowserUtils.waitForPageToLoad(10);
         vehicleTableView.nextPageBtn.click();
     }
     @Then("user should see the next page of vehicle information displayed")
     public void verifyNextPageDisplayed() {
-        BrowserUtils.waitForPageToLoad(5);
-        WebElement nextPageBtn = BrowserUtils.waitForVisibility(By.xpath("//i[@class='fa-chevron-right hide-text']"), 10);
-        assertTrue(nextPageBtn.isDisplayed());
+        BrowserUtils.waitForPageToLoad(10);
+        assertTrue(vehicleTableView.nextPageBtn.isDisplayed());
     }
 
     @When("user clicks the \"<\" button")
     public void clickPreviousPageButton() {
-
+        BrowserUtils.waitForPageToLoad(10);
         vehicleTableView.previousButton.click();
     }
 
